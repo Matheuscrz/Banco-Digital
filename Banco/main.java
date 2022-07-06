0d404fc4e6aeaf277ebabffd7628deb66c6d5c59
Matheus Lima Da Cruz <matheuslimasof.eng@gmail.com>
2022-07-06 09:14:18 -0300

    Update main.java

--- Banco/main.java
+++ Banco/main.java
@@ -4,9 +4,12 @@ import javax.swing.*;
 
 public class main {
     public static void main(String[] args) {
-        String pessoa = JOptionPane.showInputDialog("Cliente:");
-        Cliente cliente = new Cliente();
-        cliente.setNome(pessoa);
-        System.out.println("test");
+        conta cc = new ContaCorrente(011, "15/05/2000", "Aracaju", 353, "desenvolver", 2000d, true);
+        conta poupanca = new ContaPoupanca(021, "15/02/1988", "Socorro", 46466);
+        conta empresa = new ContaEmpresarial(54644);
+
+        cc.extrato();
+        poupanca.extrato();
+        empresa.extrato();
     }
 }
